0aa154567694a1724f3abb17140a8307467d34b2
Code-Mamba-254 <gichambamugacha@gmail.com>
2026-03-03 08:04:22 +0300

    Prepare app for Railway deployment 2

--- asklive/settings.py
+++ asklive/settings.py
@@ -14,11 +14,7 @@ from pathlib import Path
 import dj_database_url
 import os
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +83,9 @@ WSGI_APPLICATION = 'asklive.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
